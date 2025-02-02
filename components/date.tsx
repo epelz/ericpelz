ea1b5e569bda6fa0ba239bf8eb2082c016d0c150
Eric Pelz <github@ericpelz.com>
2025-02-02 12:59:39 -0800

    Remove date-fns library

--- components/date.tsx
+++ components/date.tsx
@@ -1,6 +1,14 @@
-import { parseISO, format } from "date-fns";
-
-export default function Date({ dateString }: { dateString: string }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+export default function DateWidget({ dateString }: { dateString: string }) {
+  // A convoluted way to render the date, without an external library.
+  const [year, month, day] = dateString.split("-").map(Number);
+  const date = new Date(year, month - 1, day); // Month is 0-based
+  return (
+    <time dateTime={dateString}>
+      {date.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      })}
+    </time>
+  );
 }
